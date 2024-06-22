c5da3ae1c7c9cb60a7fedc09f7478dc686774160
anikett-cloud <aniketweb333@gmail.com>
2024-06-22 11:22:30 +0530

    first website

---
+++ index.js
@@ -0,0 +1,30 @@
+var typing = new Typed(".animate", {
+  strings: ["Graphic Designer", "Web Designer", "Frontend Developer"],
+  typeSpeed: 120,
+  backSpeed: 20,
+  loop: true,
+});
+
+// function changeImage() {
+//   let displayImage = document.getElementById("image");
+//   if (displayImage.src.match("images/coding pic.png")) {
+//     displayImage.src = "images/pexels-cottonbro-studio-4709285.jpg";
+//   } else {
+//     displayImage.src = "images/coding pic.png";
+//   }
+// }
+
+// function changeImage() {
+//   let displayImage = document.getElementById("image");
+//   if (displayImage.style.backgroundImage.includes("images/coding pic.png")) {
+//     displayImage.style.backgroundImage =
+//       "url(images/pexels-cottonbro-studio-4709285.jpg)";
+//   } else {
+//     displayImage.style.backgroundImage = "url(images/coding pic.png)";
+//   }
+// }
+
+// alert(location.href);
+// if (confirm("want to visit")) {
+//   location.href = "https://techaniket.com/fifa/";
+// }
